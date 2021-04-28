a9c6f92f16ef9ec54d16066480b3f7c12653c5b5
eric <erichoward2141@gmail.com>
2021-04-28 15:40:43 -0500

    Updates

---
+++ precache-manifest.b8efbc6390baa93b498afb749126fc2c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37aeb3920a1ad8dd2d60d66e4572cdf",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a640ba07367db7afbaef",
+    "url": "/my-portfolio/static/css/main.74ed5d23.chunk.css"
+  },
+  {
+    "revision": "7e25b8c73f3d027cad99",
+    "url": "/my-portfolio/static/js/2.f3d56cef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.f3d56cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a640ba07367db7afbaef",
+    "url": "/my-portfolio/static/js/main.13d23b78.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "a105ee341ae407dc0a16dcb77a5b78aa",
+    "url": "/my-portfolio/static/media/LibreBaskerville-Bold.a105ee34.ttf"
+  },
+  {
+    "revision": "9ecf41ba09bcd5e10c2524b2704ddc3f",
+    "url": "/my-portfolio/static/media/LibreBaskerville-Italic.9ecf41ba.ttf"
+  },
+  {
+    "revision": "228126bc49970c1f293e1bf96e8e2604",
+    "url": "/my-portfolio/static/media/LibreBaskerville-Regular.228126bc.ttf"
+  },
+  {
+    "revision": "6eb2ebe77be273e19995e36dc057afe7",
+    "url": "/my-portfolio/static/media/about.6eb2ebe7.svg"
+  },
+  {
+    "revision": "14edbefa3fe900939ebe69ad77b51797",
+    "url": "/my-portfolio/static/media/clip-work-searches.14edbefa.svg"
+  },
+  {
+    "revision": "05304584eb993a460ce1d9fa0e58ff88",
+    "url": "/my-portfolio/static/media/cloud.05304584.svg"
+  },
+  {
+    "revision": "40d39faca6efad61fefa4427a29538fc",
+    "url": "/my-portfolio/static/media/data.40d39fac.svg"
+  },
+  {
+    "revision": "aea740a35767f6fbd5ba97068abff232",
+    "url": "/my-portfolio/static/media/game.aea740a3.svg"
+  },
+  {
+    "revision": "f97efe3a54b9743c01bd3abf7b87e282",
+    "url": "/my-portfolio/static/media/home.f97efe3a.svg"
+  },
+  {
+    "revision": "5f906fcea09e08700c5b9706713851c5",
+    "url": "/my-portfolio/static/media/message.5f906fce.svg"
+  },
+  {
+    "revision": "d0db4726a47411c7bae613be5f94167a",
+    "url": "/my-portfolio/static/media/mlai.d0db4726.svg"
+  },
+  {
+    "revision": "a694ade0df318a9751c382cdb44a651f",
+    "url": "/my-portfolio/static/media/pixeltrue-error-1.a694ade0.svg"
+  },
+  {
+    "revision": "6b588570d9d8951a79afe98545a7daf2",
+    "url": "/my-portfolio/static/media/programming.6b588570.svg"
+  },
+  {
+    "revision": "442b278844f3dacf968226aa509f82f2",
+    "url": "/my-portfolio/static/media/project.442b2788.svg"
+  },
+  {
+    "revision": "361ec9070b96036702ec7184396ee8f3",
+    "url": "/my-portfolio/static/media/robotic.361ec907.svg"
+  },
+  {
+    "revision": "4a9c88185db22a91e514211855152f67",
+    "url": "/my-portfolio/static/media/skill.4a9c8818.svg"
+  },
+  {
+    "revision": "5bf185c782d6225fd07b2a3b66089bc4",
+    "url": "/my-portfolio/static/media/web-design.5bf185c7.svg"
+  },
+  {
+    "revision": "7782534259fb777f9506147cf4146d1a",
+    "url": "/my-portfolio/static/media/web-dev.77825342.svg"
+  }
+]);
