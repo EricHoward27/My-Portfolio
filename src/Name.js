c89f8e4919b68aee4a2ff003c24b54d1f3c777b5
eric <erichoward2141@gmail.com>
2021-04-28 17:35:54 -0500

    add image in education section, add content to the project, skill, and contact sections of portfolio

--- src/Name.js
+++ src/Name.js
@@ -1,6 +1,6 @@
 const greeting = {
     title: "Hi, I'm Eric Howard",
-    logo_name: "Name",
+    logo_name: "Eric Howard",
     logo_img_show: false, // if true than show image(logo) in home page instant of text(logo)
     logo_img: "https://logo.letskhabar.com/name?name=name",
     full_name: "Eric Howard",
@@ -9,23 +9,22 @@ const greeting = {
     "I'm a motivated, quick learning, and resourceful full-stack web developer with real-world experience working effectively in teams and engaging with clients. I'm excited to pursue a position as a junior software engineer or web developer and contribute technical value to a mission-focused company.",
     Programming: true,  // if true, Show 'Programming' section in home page, if false than not show
     developer: true,  // if true, Show 'Full Stack Development' section in home page, if false than not show
-    designer: true,  // if true, Show 'designer' section in home page, if false than not show
-    cloud_infra_architecture:true,  // if true, Show 'cloud_infra_architecture' section in home page, if false than not show
-    data_Science: true,  // if true, Show 'data_Science' section in home page, if false than not show
-    ml_ai: true,  // if true, Show 'ml_ai' section in home page, if false than not show
-    Robotic: true,  // if true, Show 'Robotic' section in home page, if false than not show
-    gamedev: true,  // if true, Show 'gamedev' section in home page, if false than not show
+    designer: false,  // if true, Show 'designer' section in home page, if false than not show
+    cloud_infra_architecture:false,  // if true, Show 'cloud_infra_architecture' section in home page, if false than not show
+    data_Science: false,  // if true, Show 'data_Science' section in home page, if false than not show
+    ml_ai: false,  // if true, Show 'ml_ai' section in home page, if false than not show
+    Robotic: false,  // if true, Show 'Robotic' section in home page, if false than not show
+    gamedev: false,  // if true, Show 'gamedev' section in home page, if false than not show
 };
 
 const BlogData = {
     // this show your dev.to blog in your Portfolio blog
-    show_Blog: true,  // if true it Show your DEV.to article in Blog Section, if false than Blog Section convert to skill Section.
+    show_Blog: false,  // if true it Show your DEV.to article in Blog Section, if false than Blog Section convert to skill Section.
     devto_username: "vimal",
     Blog_Bio : [
         // this is visible in '/blog' section if show_Blog: true, else show in skill section
-        "You can Remove this section in name.js",
-        "Writing blog on Devto",
-        "my main skill is wen dev"
+        "I'm currently improving my coding skills to become a better full-stack developer",
+        "my main skill is MERN stack"
     ]
 };
 
@@ -36,27 +35,27 @@ const socialMedia = [
     {
         icon: "github",
         img: "https://logo.letskhabar.com/img?tool=github",
-        link: "Your github link"
+        link: "https://github.com/EricHoward27"
     },
     {
         icon: "gitlab",
         img: "https://logo.letskhabar.com/img?tool=gitlab",
-        link: "Your gitlab link"
+        link: "https://gitlab.com/EricHoward27"
     },
     {
         icon: "devto",
         img: "https://logo.letskhabar.com/img?tool=dev",
-        link: "Your devto link"
+        link: "https://dev.to/erichoward27"
     },
     {
         icon: "linkedin",
         img: "https://logo.letskhabar.com/img?tool=linkedin",
-        link: "Your linkedin link"
+        link: "https://www.linkedin.com/in/ehoward91/"
     },
     {
         icon: "mail",
         img: "https://logo.letskhabar.com/img?tool=mail",
-        link: "mailto:Your mail"
+        link: "mailto:howarddesignstudio27@gmail.com"
     }
 
 ];
@@ -65,12 +64,8 @@ const ProgrammingSkill = [
     // https://logo.letskhabar.com/ all logo list
     // you can use by changing tool name
     {
-        icon: "c-programming",
-        img: "https://logo.letskhabar.com/img?tool=c-programming"
-    },
-    {
-        icon: "java",
-        img: "https://logo.letskhabar.com/img?tool=java"
+        icon: "javascript",
+        img: "https://logo.letskhabar.com/img/?tool=javascript"
     },
     {
         icon: "python",
@@ -119,16 +114,28 @@ const DevSkill = [
         img: "https://logo.letskhabar.com/img?tool=yarn"
     },
     {
-        icon: "python",
-        img: "https://logo.letskhabar.com/img?tool=python"
+        icon: "django",
+        img: "https://logo.letskhabar.com/img/?tool=django"
     },
     {
-        icon: "wordpress",
-        img: "https://logo.letskhabar.com/img?tool=wordpress"
+        icon: "mongodb",
+        img: "https://logo.letskhabar.com/img/?tool=mongodb"
     },
     {
-        icon: "rust",
-        img: "https://logo.letskhabar.com/img?tool=rust"
+        icon: "postgresql",
+        img: "https://logo.letskhabar.com/img/?tool=postgresql"
+    },
+    {
+        icon: "ubuntu",
+        img: "https://logo.letskhabar.com/img/?tool=ubuntu"
+    },
+    {
+        icon: "bootstrap",
+        img: "https://logo.letskhabar.com/img/?tool=bootstrap"
+    },
+    {
+        icon: "git",
+        img: "https://logo.letskhabar.com/img/?tool=git"
     }
 
 ];
@@ -230,12 +237,7 @@ const Ml_ai_Skill = [
     {
         icon: "tensorflow",
         img: "https://logo.letskhabar.com/img?tool=tensorflow"
-    },
-    {
-        icon: "arduino",
-        img: "https://logo.letskhabar.com/img?tool=arduino"
     }
-
 ];
 
 const RoboticSkill = [
@@ -278,61 +280,53 @@ const Gamedev_Skill = [
 const projects = [
     {
         id: "1",
-        name: "project 1",
-        Description: "this is Description",
-        Code_Link: "this is code link",
-        Live_link: "this is Live link",
-        tool_used: "html,css,js",
-        img: "https://master-portfolio.js.org/src/assets/img/mplogo.png"
+        name: "Ecommerce CRUD app",
+        Description: "Ecommerce app will allow users to login and add, remove, edit items to the shopping cart using the python django stack.",
+        Code_Link: "https://github.com/EricHoward27/ecommerce-client",
+        Live_link: "https://erichoward27.github.io/ecommerce-client/",
+        tool_used: "python,postresql,react",
+        img: "."
     },
     {
         id: "2",
-        name: "project 2",
-        Description: "this is Description",
-        Code_Link: "this is code link",
-        Live_link: "this is Live link",
-        tool_used: "html,css,js",
-        img: "https://master-portfolio.js.org/src/assets/img/mplogo.png"
+        name: "Ecommerce Web app(MERN Stack)",
+        Description: "work in progress",
+        Code_Link: "https://github.com/EricHoward27/survey-client",
+        Live_link: "https://erichoward27.github.io/survey-client/",
+        tool_used: "mongodb,express,react",
+        img: "."
     },
     {
         id: "3",
-        name: "project 3",
-        Description: "this is Description",
-        Code_Link: "this is code link",
-        Live_link: "this is Live link",
-        tool_used: "html,css,js",
-        img: "https://master-portfolio.js.org/src/assets/img/mplogo.png"
+        name: "SurveyUs",
+        Description: "SurveyUs is the flagship application produced by the creatives at Mattastic-Voyage Industries. One part survey, one part the adventure of taking a survey, SurveyUs aims to streamline the surveying process by asking users tough questions and limiting their responses.",
+        Code_Link: "https://github.com/EricHoward27/survey-client",
+        Live_link: "https://erichoward27.github.io/survey-client/",
+        tool_used: "express,mongodb,react",
+        img: "."
     }
 ];
 const certificate = [
     {
         id: "1",
-        name: "certificate 1",
-        Description: "this is Description",
+        name: "Purdue University Global",
+        Description: "Associate of Applied Science with Purdue University Global",
         Link: "this is code link",
-        img: "https://master-portfolio.js.org/src/assets/img/mplogo.png"
+        img: "https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/13108535"
     },
     {
         id: "2",
-        name: "certificate 2",
-        Description: "this is Description",
-        Link: "this is code link",
-        img: "https://master-portfolio.js.org/src/assets/img/mplogo.png"
-    },
-    {
-        id: "3",
-        name: "certificate 3",
-        Description: "this is Description",
+        name: "General Assembly",
+        Description: "12-Week Software Engineer Immersive Program with General Assembly",
         Link: "this is code link",
         img: "https://master-portfolio.js.org/src/assets/img/mplogo.png"
     }
 ];
 
 const ProgrammingBio = [
-    "You can Remove this section in name.js",
-    "i know c java and python",
-    'you can edit this text from name.js',
-    "you can change below skill icons from name.js"
+    "I know javascript and python",
+    'I can build RESTful APIs and integrate data from third party APIs',
+    "I can cover front-and-back-end programming fundamentals"
 ];
 
 const DevBio = [
@@ -351,15 +345,15 @@ const DesignBio = [
 const AboutBio =[
     ' I’m currently learning Full Stack Development.',
     ' I’m currently working with Node.js, Express.js, React, Mongodb, Javascript, Flutter, etc',
-    ' I’m a student pursuing {course} in {department} from {university}',
     ' Fun fact: \'Equal is Not Always Equal in Javascript\'',
-    ' I am From {City}',
+    ' I am From Chicago',
     ' I am available on almost every social media. You can message me'
 ];
 const EducationBio =[
     ' I’m currently learning Full Stack Development.',
     ' I’m currently working with Node.js, Express.js, React, Mongodb, Javascript, Flutter, etc',
-    ' I’m a student pursuing {course} in {department} from {university}',
+    ' I’m a student pursuing Software Development in Information Technology  from  Purdue University Global',
+    ' I’m a fellow software engineer in training pursuing my certification from General Assembly coding bootcamp',
     ' Fun fact: \'Equal is Not Always Equal in Javascript\'',
 ];
 const ProjectsBio =[
@@ -367,8 +361,8 @@ const ProjectsBio =[
     ' I’m currently working with Node.js, Express.js, React, Mongodb, Javascript, Flutter, etc'
 ];
 const ContactBio =[
-    'I\'m a student pursuing {course} in {department} from {university}',
-    ' I am From {City}',
+    'I’m currently learning Full Stack Development.',
+    ' I am From Chicago',
     ' I am available on almost every social media. You can message me'
 
 ];
